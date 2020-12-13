261d64f79926607dc60ef32fff29238a060355c6
Jeremy Phillips <jeremy@uranusbytes.com>
2020-12-13 08:36:49 -0500

    Img Modal refactor

---
+++ scripts/chars.js
@@ -0,0 +1,18 @@
+var chars = {
+    'jingleringford': {
+        'name': 'Jingle Ringford',
+        'dialog': [
+            "Welcome! Hop in the gondola to take a ride up the mountain to Exit 19: Santa's castle!",
+            "Santa asked me to design the new badge, and he wanted it to look really cold - like it was frosty.",
+            "Click your badge (the snowflake in the center of your avatar) to read your objectives.",
+            "If you'd like to chat with the community, join us on Discord!",
+            "We have specially appointed Kringle Koncierges as helpers; you can hit them up for help in the #general channel!",
+            "If you get a minute, check out Ed Skoudis' official intro to the con!",
+            "Oh, and before you head off up the mountain, you might want to try to figure out what's written on that advertising bilboard.",
+            "Have you managed to read the gift list at the center?",
+            "It can be hard when things are twirly. There are tools that can help!",
+            "It also helps to select the correct twirly area."
+        ]
+    }
+
+}
